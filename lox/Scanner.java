5fa558c2613c11a25c440e9dea9b14ebf5a4e861
Kaan Komutekir <kaankom@hotmail.de>
2023-10-01 15:17:53 +0200

    feat: added method for handling identifiers in the scanner

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -1,12 +1,37 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private static final Map<String, TokenType> keywords;
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
+    }
+
+
 
     Scanner(String source) {
         this.source = source;
@@ -79,8 +104,15 @@ public class Scanner {
             // String Literals
             case '"': handleString(); break;
 
-
-            default: Lox.error(line, "Unexpected Character: '" + c +"'."); break;
+            default:
+                if(isDigit(c)) {
+                    handleNumber();
+                } else if (isAlpha(c)) {
+                    handleIdentifier();
+                } else {
+                    Lox.error(line, "Unexpected Character: '" + c +"'.");
+                }
+                break;
         }
     }
 
@@ -114,6 +146,11 @@ public class Scanner {
         return source.charAt(current);
     }
 
+    private char peekNext() {
+        if(current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
     private void handleString() {
         while(peek() != '"' && !isAtEnd()) {
             if(peek() == '\n') line++;
@@ -124,11 +161,49 @@ public class Scanner {
             return;
         }
 
-        advance(); // the closing "
+        advance(); // to the closing "
 
         // Trim the surrounding quotes
         String value = source.substring(start + 1, current -1);
         addToken(TokenType.STRING, value);
     }
 
+    private void handleNumber() {
+        while(isDigit(peek())) {
+            advance();
+        }
+
+        // Look for a fractional point
+        if(peek() == '.' && isDigit(peekNext())) {
+            // Consume the fractional point
+            advance();
+
+            while(isDigit(peek())) advance();
+        }
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void handleIdentifier() {
+        while(isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null) {
+            type = TokenType.IDENTIFIER;
+        }
+        addToken(type);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
 }
