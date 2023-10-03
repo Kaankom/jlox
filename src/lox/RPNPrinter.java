3cb1f9f01a1defe9d3b60b098a6da8e1a33d4990
Kaan Komutekir <kaankom@hotmail.de>
2023-10-03 17:25:55 +0200

    feat: added reverse polish notation printer

--- src/lox/RPNPrinter.java
+++ src/lox/RPNPrinter.java
@@ -1,6 +1,7 @@
 package lox;
 
-class AstPrinter implements Expr.Visitor<String>{
+// Prints the AST in the Reverse Polish Notation
+class RPNPrinter implements Expr.Visitor<String>{
     String print(Expr expr) {
         return expr.accept(this);
     }
@@ -26,30 +27,15 @@ class AstPrinter implements Expr.Visitor<String>{
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
-    private String parenthesize(String name, Expr... exprs) {
+    private String parenthesize(String operator, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
-        builder.append("(").append(name);
         for(Expr expr : exprs) {
             builder.append(" ");
             builder.append(expr.accept(this));
         }
-        builder.append(")");
+        builder.append(" ").append(operator);
 
         return builder.toString();
     }
-
-    // Fake parser for debugging
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(1234)),
-                new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Grouping(new Expr.Literal(45.67))
-                );
-        System.out.println(new AstPrinter().print(expression));
-    }
-
-
 }
